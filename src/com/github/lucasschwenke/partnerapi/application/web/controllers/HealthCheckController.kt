0286a058da47f4970d8d238349ac6f1275e74c79
Lucas Paixao <lucasspxo@gmail.com>
2020-09-27 10:43:56 -0300

    fix: fixed health check.

--- src/com/github/lucasschwenke/partnerapi/application/web/controllers/HealthCheckController.kt
+++ src/com/github/lucasschwenke/partnerapi/application/web/controllers/HealthCheckController.kt
@@ -3,15 +3,13 @@ package com.github.lucasschwenke.partnerapi.application.web.controllers
 import com.github.lucasschwenke.partnerapi.application.web.responses.HealthCheckResponse
 import io.ktor.application.ApplicationCall
 import io.ktor.http.HttpStatusCode
-import java.time.LocalDateTime
 
 class HealthCheckController {
 
     fun health(call: ApplicationCall): HealthCheckResponse =
         HealthCheckResponse(
             status = "Ok",
-            up = true,
-            date = LocalDateTime.now()
+            up = true
         ).also {
             call.response.status(HttpStatusCode.OK)
         }
--- src/com/github/lucasschwenke/partnerapi/application/web/responses/HealthCheckResponse.kt
+++ src/com/github/lucasschwenke/partnerapi/application/web/responses/HealthCheckResponse.kt
@@ -1,9 +1,6 @@
 package com.github.lucasschwenke.partnerapi.application.web.responses
 
-import java.time.LocalDateTime
-
 data class HealthCheckResponse(
     val status: String,
-    val up: Boolean,
-    val date: LocalDateTime
+    val up: Boolean
 )
